815a7c156ac8f45ceb4bffa8c63bb837ebd2786b
Mert Bağ <mertbag@indivitech.com>
2025-01-28 17:17:08 +0300

    Two Mental Leaps to Script Setup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,20 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import { ref, onMounted } from 'vue'
+
+let message = ref('')
+
+onMounted(() => alert('Hello, world!'))
+
+setTimeout(() => {
+  message.value = 'Lorem ipsum dolar sit amet consectetur.'
+}, 5000)
 </script>
 
 <template>
   <main>
+    <h1>{{ message }}</h1>
+
     <TheWelcome />
   </main>
 </template>
